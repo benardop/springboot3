9c43a572ed9a1401d5fd03b603d5b4427118add9
benardop <benard.oc.p@gmail.com>
2023-07-25 07:05:02 -0300

    Update home controller to load dynamic data

--- src/main/java/com/benard/learningspringboot3/springboot3/HomeController.java
+++ src/main/java/com/benard/learningspringboot3/springboot3/HomeController.java
@@ -1,16 +1,25 @@
 package com.benard.learningspringboot3.springboot3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     record Video(String name) {}
 
+    List<Video> videos = List.of(
+            new Video("Need help with your Spring Boot 3"),
+            new Video("Don't do this in your code"),
+            new Video("Secrets to fixing your code"));
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("videos", videos);
         return "index";
     }
 }
